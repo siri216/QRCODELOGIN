d5e634381be425b27b65121b73170db30e87b82b
siri216 <siripaladi7482@gmail.com>
2025-03-25 21:49:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -85,7 +85,6 @@ app.post("/fetch-user-details", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
