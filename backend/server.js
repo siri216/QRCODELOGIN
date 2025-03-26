5a5bb09a95ddba6c7fa40e3cf53ecdcc212e31ee
siri216 <siripaladi7482@gmail.com>
2025-03-26 11:59:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ app.post("/send-otp", (req, res) => {
 
     const otp = (Math.floor(100000 + Math.random() * 900000)).toString();
     otpStore[phone] = otp;
-    console.log(Generated OTP for ${phone}: ${otp});
+    console.log(`Generated OTP for ${phone}: ${otp}`);
 
     res.json({ otp, message: "OTP sent successfully." });
 });
